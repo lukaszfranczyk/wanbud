bcab0893652b5cec1f55ad61a107de4115c47dbc
Lukasz <lukasz.franczyk@gmail.com>
2015-07-20 21:09:24 +0200

    js/zmienne tlo frontu

--- js/init.js
+++ js/init.js
@@ -1,25 +1,13 @@
 window.addEventListener('load', function () {
-    var tla = ['tlo1.jpg', 'tlo2.jpg', 'tlo3.jpg', 'tlo4.jpg', 'tlo5.jpg',];
-    var czas = 3;
+    var mainPage = document.getElementById("mainPage");
+    var tla = ['url(img/tlo1.jpg)', 'url(img/tlo2.jpg)', 'url(img/tlo3.jpg)', 'url(img/tlo4.jpg)', 'url(img/tlo5.jpg)',];
+    var czas = 5;
     
     var i = 0;
     setInterval(function () {
         i++;
         i%=tla.length;
         
-        document.mainPage.style.background.url() = tla[i];
+        mainPage.style.background = tla[i];
     }, czas * 1000);
 }, false);
-
-/*window.addEventListener('load', function () { //działa zmian kolorów
-    var tla = ['red', 'green', 'blue'];
-    var czas = 3;
-    
-    var i = 0;
-    setInterval(function () {
-        i++;
-        i%=tla.length;
-        
-        document.body.style.backgroundColor = tla[i];
-    }, czas * 1000);
-}, false);*/
